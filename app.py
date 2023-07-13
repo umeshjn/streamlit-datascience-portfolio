c670d39b9038168088b5b59d3802c76930d93322
Umesh Narayanappa <u.jnms08@gmail.com>
2023-07-12 21:49:20 -0500

    Changing the ordering of the projects

--- app.py
+++ app.py
@@ -9,99 +9,6 @@ st.markdown("""
 
 st.markdown("***This web app is created to give you a glimpse into my skills and all these are built out of my own personal interest in the data and the topics***")
 
-st.markdown("### Data Visualizations (Tableau & R)")
-st.markdown("""
-    Data Visualization is a very powerful tool for presenting the data in a more insightful way to the target audience. 
-    Here in this section I present some of the visualizations or dashboards created using real world data sets using technologies like **R and Tableau**.
-""")
-
-with st.container():
-    col1, col2 = st.columns((3,3))
-    with col1:
-        st.markdown("[Covid-19 USA Cases 2020 to 2021](https://public.tableau.com/app/profile/umeshjn/viz/Covid-19CasesUSA/Covid19CaseUSA)")
-        st.write("""
-        Fight against Covid19 is into third year almost and the numbers are increasing against due to the new variant.
-        """)
-        st.image("images/Covid19 Case USA.png")
-
-    with col2:
-        st.markdown("[Covid Deaths in USA(2020 and 2021)](https://public.tableau.com/app/profile/umeshjn/viz/CovidDeathsinUSA2020and2021/CovidDeathsUSA2020and2021)")
-        st.write("""
-        So many deaths due to Covid and we have ended up treating them as just statictics which is very sad and painful if you really think about all the people who have lost their loved ones.
-        """)
-        st.image("images/coviddeaths.png")
-
-
-
-with st.container():
-    col1, col2 = st.columns((3,3))
-    with col1:
-        st.markdown("**New York State Cancer Death Rates 1976-2018**")
-        st.write("""
-        Humanity has been fighting a brave war with the cancer and the progress made since last 4-5 decades shows that there is lot of optimism. 
-        """)
-        st.image("images/nyscancerviz.PNG")
-
-    with col2:
-        st.markdown("**Drug Overdose Deaths in USA 2014-2019**")
-        st.write("""
-        Drug Overdose is one of the most biggest health concerns in America and this visualization gives you the view of Drug Overdose Death Rates 2014-2019. 
-        """)
-        st.image("images/drugoverdosedeathrates.PNG")
-
-       
-
-with st.container():
-    col1, col2 = st.columns((3,3))
-    with col1:
-        st.markdown("**Seattle Crimes Numbers Reported **")
-        st.write("""
-        Crimes in the cities are the biggest challenges for the law and order authorities. This dashboard view gives a glimpse of Seattle crimes numbers between 2008 and 2017. 
-        """)
-        st.image("images/seattlecrimes.PNG")
-
-    with col2:
-        st.markdown("**Spider Species Identified in North America**")
-        st.write("""
-        There are over 45000 spider species in the world and 7000 out of those are in North American Countries. 
-        """)
-        st.image("images/spiders_northamerica.PNG")
-
-
-with st.container():
-    col1, col2 = st.columns((3,3))
-    with col1:
-        st.markdown("[Gun Violence in America](https://public.tableau.com/app/profile/umeshjn/viz/GunViolenceinAmerica_0/GunViolence)")
-        st.write("""
-        This dashboard gives you a view of the gun violence reported across America from 2014 to 2017.
-        """)
-        st.image("images/GunViolence.png")
-
-    with col2:
-        st.markdown("[Montgomery County - 911 Calls](https://public.tableau.com/app/profile/umeshjn/viz/911-MontgomeryCounty/911-MontgomeryCounty)")
-        st.write("""
-        This dashboard gives you a view of 911 Emergency Calls made in Montgomery County, PA.
-        """)
-        st.image("images/911-MontgomeryCounty.png")
-
-
-with st.container():
-    col1, col2 = st.columns((3,3))
-    with col1:
-        st.markdown("[Bicycle Sharing in Chicago](https://public.tableau.com/app/profile/umeshjn/viz/ChicagoBicycleSharing/ChicagoBiCycleSharing)")
-        st.write("""
-        This dashboard gives you a view of bicycle hiring in Chicago from 2014 to 2017.
-        """)
-        st.image("images/ChicagoBiCycleSharing.png")
-
-    with col2:
-        st.markdown("[Airbnb Listings in NewYork City](https://public.tableau.com/app/profile/umeshjn/viz/AirbnbNewYorkCityListings_16316414938610/NyCityAirbnbLIsting)")
-        st.write("""
-        Interactive Dashboard to View the Map of the Airbnb listings in NewYork City
-        """)
-        st.image("images/NyCityAirbnbLIsting.png")
-
-
 st.markdown("### Data Apps built using Python")
 st.markdown("""
     Streamlit is one of the best frameworks available for development of the python data applications. 
@@ -231,6 +138,96 @@ with st.container():
         """)
         st.image("images/IndianStocks.PNG")
 
+st.markdown("### Data Visualizations (Tableau & R)")
+st.markdown("""
+    Data Visualization is a very powerful tool for presenting the data in a more insightful way to the target audience. 
+    Here in this section I present some of the visualizations or dashboards created using real world data sets using technologies like **R and Tableau**.
+""")
+
+with st.container():
+    col1, col2 = st.columns((3,3))
+    with col1:
+        st.markdown("[Covid-19 USA Cases 2020 to 2021](https://public.tableau.com/app/profile/umeshjn/viz/Covid-19CasesUSA/Covid19CaseUSA)")
+        st.write("""
+        Fight against Covid19 is into third year almost and the numbers are increasing against due to the new variant.
+        """)
+        st.image("images/Covid19 Case USA.png")
+
+    with col2:
+        st.markdown("[Covid Deaths in USA(2020 and 2021)](https://public.tableau.com/app/profile/umeshjn/viz/CovidDeathsinUSA2020and2021/CovidDeathsUSA2020and2021)")
+        st.write("""
+        So many deaths due to Covid and we have ended up treating them as just statictics which is very sad and painful if you really think about all the people who have lost their loved ones.
+        """)
+        st.image("images/coviddeaths.png")
+
+
+with st.container():
+    col1, col2 = st.columns((3,3))
+    with col1:
+        st.markdown("**New York State Cancer Death Rates 1976-2018**")
+        st.write("""
+        Humanity has been fighting a brave war with the cancer and the progress made since last 4-5 decades shows that there is lot of optimism. 
+        """)
+        st.image("images/nyscancerviz.PNG")
+
+    with col2:
+        st.markdown("**Drug Overdose Deaths in USA 2014-2019**")
+        st.write("""
+        Drug Overdose is one of the most biggest health concerns in America and this visualization gives you the view of Drug Overdose Death Rates 2014-2019. 
+        """)
+        st.image("images/drugoverdosedeathrates.PNG")
+
+       
+
+with st.container():
+    col1, col2 = st.columns((3,3))
+    with col1:
+        st.markdown("**Seattle Crimes Numbers Reported **")
+        st.write("""
+        Crimes in the cities are the biggest challenges for the law and order authorities. This dashboard view gives a glimpse of Seattle crimes numbers between 2008 and 2017. 
+        """)
+        st.image("images/seattlecrimes.PNG")
+
+    with col2:
+        st.markdown("**Spider Species Identified in North America**")
+        st.write("""
+        There are over 45000 spider species in the world and 7000 out of those are in North American Countries. 
+        """)
+        st.image("images/spiders_northamerica.PNG")
+
+
+with st.container():
+    col1, col2 = st.columns((3,3))
+    with col1:
+        st.markdown("[Gun Violence in America](https://public.tableau.com/app/profile/umeshjn/viz/GunViolenceinAmerica_0/GunViolence)")
+        st.write("""
+        This dashboard gives you a view of the gun violence reported across America from 2014 to 2017.
+        """)
+        st.image("images/GunViolence.png")
+
+    with col2:
+        st.markdown("[Montgomery County - 911 Calls](https://public.tableau.com/app/profile/umeshjn/viz/911-MontgomeryCounty/911-MontgomeryCounty)")
+        st.write("""
+        This dashboard gives you a view of 911 Emergency Calls made in Montgomery County, PA.
+        """)
+        st.image("images/911-MontgomeryCounty.png")
+
+
+with st.container():
+    col1, col2 = st.columns((3,3))
+    with col1:
+        st.markdown("[Bicycle Sharing in Chicago](https://public.tableau.com/app/profile/umeshjn/viz/ChicagoBicycleSharing/ChicagoBiCycleSharing)")
+        st.write("""
+        This dashboard gives you a view of bicycle hiring in Chicago from 2014 to 2017.
+        """)
+        st.image("images/ChicagoBiCycleSharing.png")
+
+    with col2:
+        st.markdown("[Airbnb Listings in NewYork City](https://public.tableau.com/app/profile/umeshjn/viz/AirbnbNewYorkCityListings_16316414938610/NyCityAirbnbLIsting)")
+        st.write("""
+        Interactive Dashboard to View the Map of the Airbnb listings in NewYork City
+        """)
+        st.image("images/NyCityAirbnbLIsting.png")
 
 st.markdown("### Data Stories Blog - RMarkdown")
 st.markdown("[Dataasana](https://www.dataasana.com/)")
